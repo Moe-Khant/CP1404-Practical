adc971c6a71f7440868046892b48a98fff3b22be
Moe Khant <moekhant@my.jcu.edu.au>
2026-02-02 00:53:43 +0800

    Add random score and determine the grade

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,19 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+import random
 
 def main():
     score = float(input("Enter score: "))
     grade = determine_score(score)
-    print(grade)
+    print(f"User score: {grade}")
 
-def determine_score(score: float):
+    random_score = random.randint(0,100)
+    random_grade = determine_score(random_score)
+    print(f"Random: {random_score} = {random_grade}")
+
+def determine_score(score):
+    """determine score based on input"""
     if score < 0 or score > 100:
         return "Invalid score"
     elif score >= 90:
