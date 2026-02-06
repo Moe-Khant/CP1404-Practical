1207cda0a1dbb614b9878781fe877823418b5a56
Moe Khant <moekhant@my.jcu.edu.au>
2026-02-06 16:53:55 +0800

    Read the name in txt file and print

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,7 +1,13 @@
 """DO from scratch"""
+from fileinput import close
 
 # Ex. 1
 out_file = open("name.txt",'w')
 name = input("Enter name: ")
 print(name,file=out_file)
 out_file.close()
+
+in_file = open("name.txt",'r')
+line = in_file.read().strip()
+print(f"Hi {line}!")
+in_file.close()
