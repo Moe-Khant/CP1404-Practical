16b9734ea4a982afda4bec1ff34bcfe9a14c64a5
Moe Khant <moekhant@my.jcu.edu.au>
2026-02-06 15:30:00 +0800

    Add generating random number code

--- prac_03/random.py
+++ prac_03/random.py
@@ -1,3 +1,8 @@
 # 7     smallest number = 5, largest 20
 # 3     smallest number = 3, largest 9  No
 # 4.7503102350488255    smallest number = 2.5, largest 5.5
+
+import random
+
+random_number = random.randint(1,100)
+print(random_number)
