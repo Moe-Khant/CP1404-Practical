4cdccab25d6e8c2bee4c4482fd17ba57d4570ba8
Moe Khant <moekhant@my.jcu.edu.au>
2026-02-19 01:55:57 +0800

    generate random picks and sorted

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,4 +4,5 @@ NUMBERS_IN_LINE = 6
 number_of_picks = int(input("How many quick picks? "))
 
 for i in range(number_of_picks):
-    print([random.randint(1,45) for number in range(6) ])
+    picks = [random.randint(1,45) for number in range(6)]
+    print(*sorted(picks))
