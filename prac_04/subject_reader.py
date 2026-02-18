893915b5f656130ebfd22bd70a25560cc9f9ee6b
Moe Khant <moekhant@my.jcu.edu.au>
2026-02-18 22:47:57 +0800

    Return data as a nested list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -15,16 +15,20 @@ def main():
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    new_data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         # Make the number an integer as part of a new, poorly named, list
         data = [parts[0], parts[1], int(parts[2])]
-        print(data)  # See if that worked
-        print("----------")
+        # print(data)  # See if that worked
+        new_data.append(data)
+        # print("----------")
+    return new_data
+
     input_file.close()
 
 
