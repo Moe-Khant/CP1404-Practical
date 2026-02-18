2e53062af8060020a865726eb74c4c44a6fb9abb
Moe Khant <moekhant@my.jcu.edu.au>
2026-02-18 22:16:08 +0800

    Refactor the printing codes into functions

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -2,6 +2,7 @@
 CP1404/CP5632 Practical
 Starter code for cumulative total income program
 """
+from typing import Any
 
 
 def main():
@@ -13,13 +14,16 @@ def main():
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    display_report(incomes, number_of_months)
+
+
+def display_report(incomes: list[Any], number_of_months: int):
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
         print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
-
 
 
 main()
